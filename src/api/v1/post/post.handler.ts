09ad94de2962f4e6d84ead04f5783f756395777f
tesilio <lodcoco@gmail.com>
2023-07-27 22:59:00 +0900

    feat: Post 생성 핸들러 1차 구현

--- src/api/v1/post/post.handler.ts
+++ src/api/v1/post/post.handler.ts
@@ -1,21 +1,37 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { SequelizeClient } from '../../../component/sequelize-client';
+import middy from '@middy/core';
+import errorLogger from '@middy/error-logger';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { PostController } from './post.controller';
 import { PostCreateDto } from './dto/post-create.dto';
 import { UtilityComponent } from '../../../component/utility.component';
+import { SequelizeClient } from '../../../component/sequelize-client';
+import { instanceToPlain } from 'class-transformer';
 
 /**
  *
  * @param event
  * @returns {Promise<{input: any, message: string}>}
  */
-const createPostHandler = async (event: APIGatewayProxyEvent) => {
+const createPost = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const sequelizeClient = SequelizeClient.getInstance();
   sequelizeClient.initPools();
-  const parsedBody = UtilityComponent.eventBodyParser(event.body);
-  const result = await PostController.create(parsedBody as PostCreateDto); // todo: 응답값 정리, 에러 핸들링
+  const parsedBody = UtilityComponent.eventBodyParser(event.body) as any;
+  const postCreateDto = new PostCreateDto();
+  postCreateDto.title = parsedBody?.title || '젬옥';
+  postCreateDto.contents = parsedBody?.title || '냉무';
+  const postResponseDto = await PostController.create(postCreateDto);
+  const body = instanceToPlain(postResponseDto);
   await sequelizeClient.close();
-  return result;
+  return {
+    statusCode: 200,
+    body: body as any,
+  };
 };
+
+const createPostHandler = middy(createPost).use(
+  errorLogger({
+    logger: console.log,
+  }),
+);
 
 export { createPostHandler };
